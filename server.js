2005151a85115e14a156906bc22d0d57e8049d9c
Milena Garcés <milenagarces@Milenas-MacBook-Pro.local>
2017-12-10 16:27:07 -0500

    mongolab config3

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_f59nkcd9:nbm4h6tm2j89v72hr1ve704ou6@ds135966.mlab.com:35966/heroku_f59nkcd9";
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
